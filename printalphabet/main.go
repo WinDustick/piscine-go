15007344e20706ac6a8968ca232ef09a7820566b
WinDustick <windustick@gmail.com>
2025-07-18 20:10:26 +0500

    quest 2, alphabets

---
+++ printalphabet/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+
+	var aRune rune = 'a'
+	z01.PrintRune(aRune)
+
+	var bRune rune = 'b'
+	z01.PrintRune(bRune)
+
+	var cRune rune = 'c'
+	z01.PrintRune(cRune)
+
+	var dRune rune = 'd'
+	z01.PrintRune(dRune)
+
+	var eRune rune = 'e'
+	z01.PrintRune(eRune)
+
+	var fRune rune = 'f'
+	z01.PrintRune(fRune)
+
+	var gRune rune = 'g'
+	z01.PrintRune(gRune)
+
+	var hRune rune = 'h'
+	z01.PrintRune(hRune)
+
+	var iRune rune = 'i'
+	z01.PrintRune(iRune)
+
+	var jRune rune = 'j'
+	z01.PrintRune(jRune)
+
+	var kRune rune = 'k'
+	z01.PrintRune(kRune)
+
+	var lRune rune = 'l'
+	z01.PrintRune(lRune)
+
+	var mRune rune = 'm'
+	z01.PrintRune(mRune)
+
+	var nRune rune = 'n'
+	z01.PrintRune(nRune)
+
+	var oRune rune = 'o'
+	z01.PrintRune(oRune)
+
+	var pRune rune = 'p'
+	z01.PrintRune(pRune)
+
+	var qRune rune = 'q'
+	z01.PrintRune(qRune)
+
+	var rRune rune = 'r'
+	z01.PrintRune(rRune)
+
+	var sRune rune = 's'
+	z01.PrintRune(sRune)
+
+	var tRune rune = 't'
+	z01.PrintRune(tRune)
+
+	var uRune rune = 'u'
+	z01.PrintRune(uRune)
+
+	var vRune rune = 'v'
+	z01.PrintRune(vRune)
+
+	var wRune rune = 'w'
+	z01.PrintRune(wRune)
+
+	var xRune rune = 'x'
+	z01.PrintRune(xRune)
+
+	var yRune rune = 'y'
+	z01.PrintRune(yRune)
+
+	var zRune rune = 'z'
+	z01.PrintRune(zRune)
+	z01.PrintRune('\n')
+
+}
